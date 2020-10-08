592018e7ebc4dc952c91d04f783249609a8b0c4c
NguyenThiTuyenHY <70678739+NguyenThiTuyenHY@users.noreply.github.com>
2020-10-08 09:09:39 +0700

    change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-
+import {Routes, RouterModule} from '@angular/router';
+const approuter : Routes = [
+  {
+    path:'',
+    loadChildren:()=>import('./admin/main/main.module').then((m)=>m.MainModule)
+  }
+]
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(approuter)
   ],
   providers: [],
   bootstrap: [AppComponent]
